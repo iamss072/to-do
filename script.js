2d7a9b6e4c6ee88cea074b4c0a4c3b7f031d382c
iamss072 <iams072@gmail.com>
2021-07-01 03:49:54 +0530

    empty task handeled

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-let plbt=document.querySelector(".material-icons");
-let taksitem=document.querySelector(".task-container");
-let taskadding=false;
-let input=document.querySelector(".task-input");
-let hr=document.querySelector(".headwrapper");
-plbt.addEventListener("click",function(e){
+let plbt = document.querySelector(".material-icons");
+let taksitem = document.querySelector(".task-container");
+let taskadding = false;
+let input = document.querySelector(".task-input");
+let hr = document.querySelector(".headwrapper");
+plbt.addEventListener("click", function (e) {
     // console.log(e);
-    if(!taskadding){
+    if (!taskadding) {
         plbt.classList.add("clicked");
         taksitem.classList.remove("togclass");
-        taskadding=true;
+        taskadding = true;
         input.focus();
     }
-    else{
+    else {
         plbt.classList.remove("clicked");
         taksitem.classList.add("togclass");
-        taskadding=false;
+        taskadding = false;
     }
-    
+
 })
-let sub=document.querySelector(".submit");
-sub.addEventListener("click",function(e){
-    
+let sub = document.querySelector(".submit");
+sub.addEventListener("click", function (e) {
+
     console.log(input.value);
-    let item=`<div class="task-item">
+    if (input.value != "") {
+        let item = `<div class="task-item">
                 <div class="task">${input.value}</div>
                 <div class="material-icons completion">done_all</div>
             </div>`;
-    document.querySelector(".tasklist").innerHTML+=item;
-    input.value="";
-    input.focus();
+        document.querySelector(".tasklist").innerHTML += item;
+        input.value = "";
+        
+        input.focus();
+    }
+    else{
+        let alertdiv=document.createElement("div");
+        alertdiv.innerText="Empty task can't be added";
+        alertdiv.classList.add("empty-alert");
+        hr.appendChild(alertdiv);
+        setTimeout(() => {
+            alertdiv.remove();
+        }, 1000);
+        input.focus();
+    }
+
 })
-let todel=document.querySelectorAll(".task-item");
+let todel = document.querySelectorAll(".task-item");
 console.log(todel);
-for(let i=0;i<todel.length;i++){
-    todel[i].addEventListener("click",function(e){
+for (let i = 0; i < todel.length; i++) {
+    todel[i].addEventListener("click", function (e) {
         console.log(e.target);
     })
 }
