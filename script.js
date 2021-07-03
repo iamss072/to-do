cd449acdc7685c38f0de53b24b22844c09f7014b
iamss072 <iams072@gmail.com>
2021-07-04 02:51:43 +0530

    date added

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let taksitem = document.querySelector(".task-container");
 let taskadding = false;
 let input = document.querySelector(".task-input");
 let hr = document.querySelector(".headwrapper");
+let list=document.querySelector(".tasklist");
 plbt.addEventListener("click", function (e) {
-    // console.log(e);
     if (!taskadding) {
         plbt.classList.add("clicked");
         taksitem.classList.remove("togclass");
@@ -19,15 +19,21 @@ plbt.addEventListener("click", function (e) {
 
 })
 let sub = document.querySelector(".submit");
+input.addEventListener("keypress",function(e){
+    if(e.key=="Enter"){
+        addtask();
+    }
+})
 sub.addEventListener("click", function (e) {
-
-    console.log(input.value);
+    addtask();
+})
+function addtask(){
+    let now=JSON.stringify(new Date());
+    // console.log(now);
+    let ele=now.split("T");
+    console.log(ele);
     if (input.value != "") {
-        let item = `<div class="task-item">
-                <div class="task">${input.value}</div>
-                <div class="material-icons completion">done_all</div>
-            </div>`;
-        document.querySelector(".tasklist").innerHTML += item;
+        createtask(input.value,ele[0]);
         input.value = "";
         
         input.focus();
@@ -42,12 +48,31 @@ sub.addEventListener("click", function (e) {
         }, 1000);
         input.focus();
     }
+}
 
-})
-let todel = document.querySelectorAll(".task-item");
-console.log(todel);
-for (let i = 0; i < todel.length; i++) {
-    todel[i].addEventListener("click", function (e) {
-        console.log(e.target);
+function createtask(inval,date){
+    // let item = `<div class="task-item">
+    //             <div class="task">${input.value}</div>
+    //             <div class="material-icons completion">done_all</div>
+    //         </div>`;
+    let tasklistitem=document.createElement('div');
+    tasklistitem.classList.add('task-item');
+    let newtask=document.createElement('div');
+    newtask.classList.add('task');
+    newtask.innerHTML=inval;
+    let datecard=document.createElement('div');
+    datecard.innerHTML=date;
+    let delbut=document.createElement('div');
+    delbut.classList.add('material-icons');
+    delbut.classList.add('completion');
+    delbut.innerHTML="done_all";
+    // console.log(tasklistitem);
+    tasklistitem.appendChild(datecard);
+    tasklistitem.appendChild(newtask);
+    tasklistitem.appendChild(delbut);
+    
+    list.appendChild(tasklistitem);
+    delbut.addEventListener("click",function(e){
+        this.parentNode.remove();
     })
 }
